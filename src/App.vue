c57dd5d88ab839f7538e1cf0f95a8ac052ccda6c
VictorSandbox <victor.w.cairo@gmail.com>
2021-07-18 15:52:46 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gray-200 w-full h-screen flex flex-col items-center p-4">
+
+   <input type="text" placeholder="Add an item" v-model="value" />
+
+   <button @click.prevent="addItem">Add</button>               
+
+   <List :items="items"></List>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    List,
+  },
+  data() {
+    return {
+      value: '' ,
+      items: [
+        'item1', 
+        'item2'
+      ]
+    }
+  },
+  methods: {
+    addItem: function () {
+      this.items.push(this.value);
+      // this.items.unshift(this.item);
+      this.value = '';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/styles.css';
+  /* require('@/assets/styles/main.css') */
+  /* https://stackoverflow.com/questions/43784202/how-to-include-css-files-in-vue-2 */
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+      name: 'List',   
+      props: ['items']
+    }
+</script>
+
+<style></style>
